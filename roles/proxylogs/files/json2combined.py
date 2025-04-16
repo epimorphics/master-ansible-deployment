3be402d807e8e727f7c94517999bf30256e8812d
andrew-pickin-epi <Andrew.Pickin@epimorphics.com>
2025-04-16 14:39:29 +0000

    Nginx proxy support

--- roles/proxylogs/files/json2combined.py
+++ roles/proxylogs/files/json2combined.py
@@ -8,18 +8,17 @@ for line in fileinput.input():
 
     l = json.loads(line)
 
-    print('{}:{} {} {} [{}] "{} {} {}" {} {} "{}" "{}"'.format(
+    print('{}:- {} {} [{}] "{} {} {}" {} {} "{}" "{}"'.format(
         l.get('host', '-'),
-        l.get('post', '-'),
-        l.get('log_client', l.get('log_host', '-')),
+        l.get('log_client_ip', l.get('log_client', '-')),
         l.get('log_user', '-'),
         l.get('log_localtime', '-'),
         l.get('log_method', '-'),
-        l.get('log_path', '-'),
+        l.get('log_path', l.get('log_request_uri', '-')),
         l.get('log_protocol', '-'),
-        l.get('log_status', '-'),
+        l.get('log_code', l.get('log_status', '-')),
         l.get('log_size', '-'),
-        l.get('log_referrer', '-'),
+        l.get('log_referrer', l.get('log_referer', '-')),
         l.get('log_agent', '-')
         )
     )
